454f510c30a746e9141980b8ac5c07243d5e8475
Trenton Ornelas <trentonornelas@gmail.com>
2022-07-30 18:15:24 -0500

    Post update

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/F4llenPotAto/golang-microservice-testing
+
+go 1.18
--- main.go
+++ main.go
@@ -4,15 +4,20 @@ import (
 	"log"
 	"net/http"
 	"os"
+
+	"github.com/F4llenPotAto/golang-microservice-testing/handlers"
 )
 
 func main() {
 	l := log.New(os.Stdout, "product-api", log.LstdFlags)
+
 	hh := handlers.NewHello(l)
+	gh := handlers.NewGoodbye(l)
 
 	sm := http.NewServeMux()
 	sm.Handle("/", hh)
+	sm.Handle("/goodbye", gh)
 
-	http.ListenAndServe("localhost:9090", nil)
+	http.ListenAndServe("localhost:9090", sm)
 
 }
